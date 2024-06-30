7bd5330235398d0c518a35b5345d6e21f6c3bf87
Ryan Wüest <ryan.wueest@protonmail.com>
2024-06-30 18:57:17 +0200

    Fix treesitter

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -7,7 +7,7 @@
     };
     treesitter-context = {
       enable = true;
-      settings = {max_lines = 2;};
+      #settings = {max_lines = 2;};
     };
     rainbow-delimiters.enable = true;
   };
