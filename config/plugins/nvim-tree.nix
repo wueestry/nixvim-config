dfd2b8dd2f15c1a7244d65b872cc1a3b37943341
Ryan Wüest <ryan.wueest@protonmail.com>
2024-07-15 10:44:43 +0200

    Add nvim tree keymap

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -4,4 +4,16 @@
     openOnSetupFile = true;
     autoReloadOnWrite = true;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>ee";
+      action = "<cmd>NvimTreeToggle<CR>";
+      options = {
+        silent = true;
+        desc = "Toggle file explorer";
+      };
+    }
+  ];
 }
